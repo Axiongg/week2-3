035a6f0a16706059660aa78bfecdf500423b9df0
Axiongg <161007911+Axiongg@users.noreply.github.com>
2024-03-05 17:34:30 +0800

    Add files via upload

---
+++ even.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  5 16:55:07 2024
+
+@author: THULIB
+"""
+
+a=int(input("請輸入一個數"))
+if a%2==0:
+   print(a,"是偶數")
+else:
+   print(a,"不是偶數")
+      
